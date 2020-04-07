a08937efe2eeb2aa7106068a7aaca4749d50a22f
Anthony <hanthony1999@yahoo.com>
2020-04-06 19:51:18 -0500

    updated

--- BalloonParty/BalloonParty.WebUI/Startup.cs
+++ BalloonParty/BalloonParty.WebUI/Startup.cs
@@ -8,8 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using BalloonParty.DataAccess.SQLData;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace BalloonParty.WebUI
 {
@@ -27,8 +28,8 @@ namespace BalloonParty.WebUI
         {
             services.AddControllersWithViews();
 
-            // services.AddDbContext<BalloonParty.DataAccess.SQLData.BalloonPartyContext>(options =>
-            //     options.UseSqlServer(Configuration.GetConnectionString("BPDatabase")));
+            services.AddDbContext<BalloonParty.DataAccess.SQLData.BalloonPartyContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("BalloonPartyContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
